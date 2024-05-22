5045f312dd091174d9d780558cab94a868b1a362
dev-avology <devavology12@gmail.com>
2024-05-22 13:03:40 +0530

    SDds

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,6 @@ function closeNav() {
     document.getElementById("mySidenavs").style.width = "0";
 }
 
-$(document).ready(function () {
-    // Check if the modal has been shown before
-    if (!localStorage.getItem('modalShown')) {
-        $('#myModal').modal('show'); // Trigger the modal
-        localStorage.setItem('modalShown', true); // Set flag to indicate modal has been shown
-    }
-});
+
 
 
